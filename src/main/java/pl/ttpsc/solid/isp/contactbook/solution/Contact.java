9c944a3a400b0f50d70df0cc15886fe9a56f36fe
adach-203844 <szymona96@gmail.com>
2018-03-26 15:32:59 +0200

    Zadanie 5

---
+++ src/main/java/pl/ttpsc/solid/isp/contactbook/solution/Contact.java
@@ -0,0 +1,14 @@
+package pl.ttpsc.solid.isp.contactbook.solution;
+
+public class Contact implements EmailSender, PhoneCaller {
+
+  @Override
+  public void sendEmail() {
+    // ...
+  }
+
+  @Override
+  public void call() {
+
+  }
+}
---
+++ src/main/java/pl/ttpsc/solid/isp/contactbook/solution/EmailSender.java
@@ -0,0 +1,6 @@
+package pl.ttpsc.solid.isp.contactbook.solution;
+
+public interface EmailSender {
+
+  void sendEmail();
+}
